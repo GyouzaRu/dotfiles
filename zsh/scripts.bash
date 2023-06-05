8af41b6340604ceca8e775ae27021dd184bef593
lujw2 <lujw2@lenovo.com>
2023-06-05 12:43:22 +0800

    Modify lazygit and proxy scripts
    Modify lazygit to edit file directly
    Make proxy and wslproxy together

--- zsh/scripts.bash
+++ zsh/scripts.bash
@@ -1,19 +1,20 @@
 # proxy setting
 proxy(){
-    export ALL_PROXY="sock5://127.0.0.1:7890"
-    export all_proxy="sock5://127.0.0.1:7890"
-    export http_proxy="http://127.0.0.1:7890"
-    export https_proxy="http://127.0.0.1:7890"
-    echo "start proxy"
-}
-
-wslproxy(){
-    wsl_hostip=$(cat /etc/resolv.conf |grep "nameserver" |cut -f 2 -d " ")
-    export ALL_PROXY="sock5://$wsl_hostip:7890"
-    export all_proxy="sock5://$wsl_hostip:7890"
-    export http_proxy="http://$wsl_hostip:7890"
-    export https_proxy="http://$wsl_hostip:7890"
-    echo "start wsl proxy, $wsl_hostip"
+    host_ip=$(cat /etc/resolv.conf |grep "nameserver" |cut -f 2 -d " ")
+    host_ip_part=$(cut -f 1-3 -d "." <<< $host_ip)
+    if [ $host_ip_part = "127.0.0" ]; then
+        export ALL_PROXY="sock5://127.0.0.1:7890"
+        export all_proxy="sock5://127.0.0.1:7890"
+        export http_proxy="http://127.0.0.1:7890"
+        export https_proxy="http://127.0.0.1:7890"
+        echo "start proxy"
+    else
+        export ALL_PROXY="sock5://$wsl_hostip:7890"
+        export all_proxy="sock5://$wsl_hostip:7890"
+        export http_proxy="http://$wsl_hostip:7890"
+        export https_proxy="http://$wsl_hostip:7890"
+        echo "start wsl proxy, $wsl_hostip"
+    fi
 }
 
 unproxy(){
